7aaaa05d3674b7ffe7f63909d940b5c11ae7c986
NamesSid <Rkkeys@hotmail.com>
2021-05-07 23:53:12 +0200

    last update :100:

--- main.c
+++ main.c
@@ -1,18 +1,54 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "USART.h"
+
+#define S_MAX 4500UL
+#define S_MIN 800UL
 
 //Port where DHT sensor is connected
 #define DHT_DDR DDRB
 #define DHT_PORT PORTB
 #define DHT_PIN PINB
-#define DHT_INPUTPIN 0
+#define DHT_INPUTPIN 2
 
 //timeout retries
 #define DHT_TIMEOUT 200
+
+ISR (PCINT0_vect){ //External interrupt_zero ISR
+	if (!(PINB & (1<<PINB0)))
+	{
+		PORTD |= (1<<PORTD7);
+	}
+	else{
+		PORTD &= ~(1<<PORTD7);
+	}
+}
+
+static inline void Timer1Servo(){
+	TCCR1A = (1 << COM1A1)|(1 <<WGM11);
+	TCCR1B = (1 << WGM13)|(1 << WGM12)|(1 << CS11);
+	// TCNT1 = 0;
+	ICR1 = 40000;
+	DDRB |= (1 << DDB1);
+}
+
+void iniADC(uint16_t channel){
+	ADMUX = (1 << REFS0)|channel;
+	ADCSRA = (1 << ADEN)|(1 <<ADPS1)|(1 << ADPS0)|(1 << ADPS2)|(1 << ADIE);
+	ADCSRA |= (1 << ADSC);
+}
+
+uint16_t read_adc(uint8_t channel){
+	//ADMUX &= 0xF0; //Clear the older channel that was read
+	ADMUX |= channel; //Defines the new ADC channel to be read
+	ADCSRA |= (1<<ADSC); //Starts a new conversion
+	while(ADCSRA & (1<<ADSC)); //Wait until the conversion is done
+	return ADC; //Returns the ADC value of the chosen channel
+}
 
 int8_t dht_GetTemp(int8_t *temperature, int8_t *humidity) {
 	uint8_t bits[5];
@@ -88,31 +124,55 @@ int8_t dht_GetTemp(int8_t *temperature, int8_t *humidity) {
 	return -1;
 }
 
-int8_t dht_GetTempUtil(int8_t *temperature, int8_t *humidity) {
-	return dht_GetTemp(temperature, humidity);
-}
-
 int8_t temperature_int = 0;
 int8_t humidity_int = 0;
 char temperature_str[8];
 char humidity_str[8];
 
-
-int main(void) {
-  initUSART();
-  // ------ Event loop ------ //
-  while (1) {
-	  if (dht_GetTempUtil(&temperature_int, &humidity_int) != -1) {
-		  itoa(temperature_int,temperature_str,10);
-		  printString("Temperature: ");
-		  printString(temperature_str);
-		  printString(" C\r\n");
-		  itoa(humidity_int,humidity_str,10);
-		  printString("Humidity: ");
-		  printString(humidity_str);
-		  printString(" %\r\n");
-	  }
-	_delay_ms(500);
-  }                                                  /* End event loop */
-  return 0;
+int8_t dht_GetTempUtil(int8_t *temperature, int8_t *humidity) {
+	return dht_GetTemp(temperature, humidity);
 }
+
+int main(void){
+	
+	initUSART();
+	PORTB |= (1<<PORTB0);//Innput pull up på poert B0
+	DDRD |= (1<<DDD7);//Pinne PD7 er output
+	DDRB &= ~(1<<DDB0);//pinne B0 er innput
+	PCMSK0 = (1<<PCINT0)|(1<<PCINT1);//
+	PCICR |= (1<<PCIE0);//
+
+	uint16_t adc_value;
+	iniADC(0);
+	iniADC(1);
+	
+	
+	
+	char buffer[10];
+	Timer1Servo();
+	sei();//Kalle opp interuppt
+	
+	while (1)
+	{
+				
+				ADCSRA |= (1 << ADSC);
+				while(!(ADCSRA |= (1 << ADSC))){}
+				OCR1A = (ADC*(S_MAX-S_MIN)/1024 + S_MIN);
+				
+				if (dht_GetTempUtil(&temperature_int, &humidity_int) != -1) {
+					itoa(temperature_int,temperature_str,10);
+					printString("Temperature: ");
+					printString(temperature_str);
+					printString(" C\r\n");
+					itoa(humidity_int,humidity_str,10);
+					printString("Humidity: ");
+					printString(humidity_str);
+					printString(" %\r\n");
+				}
+				adc_value = read_adc(0);
+				itoa(adc_value, buffer, 10);
+				printString(buffer);
+				_delay_ms(1000);
+				printString(" ");
+	} 
+}
